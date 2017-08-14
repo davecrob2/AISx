18553bfff3626b11ba40d59d6cb34afec20751b7
davecrob2 <davecrob2@gmail.com>
2017-08-13 23:17:05 -0500

    .json dictionaries added

--- AISx.py
+++ AISx.py
@@ -5,26 +5,34 @@ Created on Sun Aug 13 15:28:06 2017
 @author: davecrob2
 """
 import csv
+from je_num import code_gen
+import json
 
-gl ={}
 JE = {}
 
 def gl_add():
     password=input("Enter a Password")
+    
+    #Poor man's security
     if password == "Password":
+        
+        #Requests Account Name and Number to be added
         Account_Name=input("Enter an Account Name")
         Account_Number=input("Enter an Account Number")
-        gl[Account_Name]=Account_Number
-        with open("C:\\Users\\davecrob2\\Documents\\GitHub\\AIS\\gl.csv",'w') as gl_csv:
-            writer =csv.writer(gl_csv)
-            for key, value in gl.items():
-                writer.writerow([key,value])
-    return gl
+    
+    
+    #Writes Account Name and Account Number inputs into a .json dictionary
+        with open('gl.json','r') as fp:
+            json_data=json.load(fp)
+            json_data[Account_Name]=int(Account_Number)
+        with open('gl.json','w') as fp:
+            fp.write(json.dumps(json_data))
 
 def JE_writer():
+    code=code_gen()
     Account_Number=input("Enter an Account Number")
     Amount =input("Enter an Amount")
-    JE[Account_Number]=Amount
+    JE[code]=[Account_Number,Amount]
     with open("C:\\Users\\davecrob2\\Documents\\GitHub\\AIS\\jeextract.csv",'w') as je_csv:
         writer =csv.writer(je_csv)
         for key, value in JE.items():
